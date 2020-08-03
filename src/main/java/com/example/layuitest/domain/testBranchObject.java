ee28aab78c9498857ee892f6ae8613f15cb23f5b
JackM0rrison <1416159010@qq.com>
2020-08-03 09:19:58 +0800

    branch test

--- src/main/java/com/example/layuitest/domain/testBranchObject.java
+++ src/main/java/com/example/layuitest/domain/testBranchObject.java
@@ -1,4 +1,12 @@
 package com.example.layuitest.domain;
 
 public class testBranchObject {
+
+    private String name;
+
+    private String id;
+
+    private String column;
+
+    private String row;
 }
